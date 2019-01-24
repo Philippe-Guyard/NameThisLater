6735316a72e362a916662f3f72d4a7423a37df1b
KeepCalmAndLoveML <32905904+KeepCalmAndLoveML@users.noreply.github.com>
2019-01-24 18:38:44 +0300

    Add files via upload

---
+++ IOSFirstGame/Content/Test/Blocks.tsx
@@ -0,0 +1,64 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Blocks" tilewidth="32" tileheight="32" tilecount="256" columns="16">
+ <image source="Blocks.png" trans="ff00ff" width="512" height="512"/>
+ <tile id="56">
+  <animation>
+   <frame tileid="48" duration="100"/>
+   <frame tileid="49" duration="100"/>
+   <frame tileid="50" duration="100"/>
+   <frame tileid="51" duration="100"/>
+   <frame tileid="52" duration="100"/>
+   <frame tileid="53" duration="100"/>
+   <frame tileid="54" duration="100"/>
+   <frame tileid="55" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="72">
+  <animation>
+   <frame tileid="64" duration="100"/>
+   <frame tileid="65" duration="100"/>
+   <frame tileid="66" duration="100"/>
+   <frame tileid="67" duration="100"/>
+   <frame tileid="68" duration="100"/>
+   <frame tileid="69" duration="100"/>
+   <frame tileid="70" duration="100"/>
+   <frame tileid="71" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="88">
+  <animation>
+   <frame tileid="80" duration="100"/>
+   <frame tileid="81" duration="100"/>
+   <frame tileid="82" duration="100"/>
+   <frame tileid="83" duration="100"/>
+   <frame tileid="84" duration="100"/>
+   <frame tileid="85" duration="100"/>
+   <frame tileid="86" duration="100"/>
+   <frame tileid="87" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="104">
+  <animation>
+   <frame tileid="96" duration="100"/>
+   <frame tileid="97" duration="100"/>
+   <frame tileid="98" duration="100"/>
+   <frame tileid="99" duration="100"/>
+   <frame tileid="100" duration="100"/>
+   <frame tileid="101" duration="100"/>
+   <frame tileid="102" duration="100"/>
+   <frame tileid="103" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="120">
+  <animation>
+   <frame tileid="112" duration="100"/>
+   <frame tileid="113" duration="100"/>
+   <frame tileid="114" duration="100"/>
+   <frame tileid="115" duration="100"/>
+   <frame tileid="116" duration="100"/>
+   <frame tileid="117" duration="100"/>
+   <frame tileid="118" duration="100"/>
+   <frame tileid="119" duration="100"/>
+  </animation>
+ </tile>
+</tileset>
